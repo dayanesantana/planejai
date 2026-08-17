90c981cdb488894fc6291ac0dc157e0de52fa226
Dayane Santana <dayanesantana59@gmail.com>
2026-08-17 15:15:02 -0300

    Alternando tema

--- src/components/shared/Header.tsx
+++ src/components/shared/Header.tsx
@@ -1,12 +1,17 @@
-import { Clock, TrendingUp, Wallet } from 'lucide-react'
+import { Clock, Moon, Sun, TrendingUp, Wallet } from 'lucide-react'
 import { useNavigate } from 'react-router-dom'
 import { Button } from './Button'
+import { useTheme } from '@/hooks/UseTheme'
+import { Divider } from './Divider'
 
 export function Header() {
   const navigate = useNavigate()
+  const { theme, toggleTheme } = useTheme()
+
   return (
     <header className="border-b border-(--border) px-6 py-3">
       <nav className="flex items-center justify-between">
+        {/* Logo */}
         <div className="flex items-center gap-2">
           <div className="bg-primary flex h-9 w-9 items-center justify-center rounded-full">
             <Wallet size={20} className="text-primary-foreground" />
@@ -16,7 +21,9 @@ export function Header() {
             <span className="font-extrabold">.ai</span>
           </span>
         </div>
-         <div className="flex items-center gap-1">
+
+        {/* Actions Buttons */}
+        <div className="flex items-center gap-1">
           <Button
             variant="secondary"
             icon={TrendingUp}
@@ -31,6 +38,13 @@ export function Header() {
           >
             <span className="hidden sm:inline">Histórico</span>
           </Button>
+          <Divider orientation="vertical" />
+          <Button
+            aria-label={`Mudar para tema ${theme === 'light' ? 'escuro' : 'claro'}`}
+            variant="ghost"
+            icon={theme === 'light' ? Moon : Sun}
+            onClick={toggleTheme}
+          />
         </div>
       </nav>
     </header>
